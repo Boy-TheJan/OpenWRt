9f320f82e40650b146324785c658cc0fe26e34b3
Boy-TheJan <962288791@qq.com>
2026-06-14 12:20:34 +0800

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone --depth 1 https://github.com/OrayOS/OpenOray package/luci-app-pgyvpn
+
+
+# UnblockNeteaseMusic
+git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
+
